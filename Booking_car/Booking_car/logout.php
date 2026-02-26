4e053196bc2ff498704df4adb1cc6d92654879d1
GOLFZILLA-2507 <golfkidsada250740@gmail.com>
2026-02-26 19:25:51 +0700

    first upload

--- Booking_car/Booking_car/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // เริ่มต้น session
-session_unset(); // ล้างข้อมูลทั้งหมดใน session
-session_destroy(); // ทำลาย session
-header("Location: login.php"); // เปลี่ยนเส้นทางไปยังหน้าเข้าสู่ระบบ
-exit();
-?>
